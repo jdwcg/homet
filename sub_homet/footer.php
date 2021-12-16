617280cf68616ef1eb638756f61dc315e8c4cb7e
jdwcg <73451502+jdwcg@users.noreply.github.com>
2021-12-16 16:17:07 +0900

    4

---
+++ sub_homet/footer.php
@@ -0,0 +1,32 @@
+<div class="footer-dark" style="max-width: 640px;margin: 0 auto;">
+    <footer>
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Web design</a></li>
+                        <li><a href="#">Development</a></li>
+                        <li><a href="#">Hosting</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">Company</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                </div>
+            </div>
+            <p class="copyright">Curves Korea © 2020</p>
+        </div>
+    </footer>
+</div>
+<script src="sub/assets/js/jquery.min.js"></script>
+<script src="sub/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="sub/assets/js/chart.min.js"></script>
+<script src="sub/assets/js/bs-init.js"></script>
+</body>
+
+</html>
